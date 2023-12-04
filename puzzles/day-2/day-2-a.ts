9fed22653d67feabcc5b215ff44c2c29a162dd01
Klaas Cuvelier <cuvelierklaas@gmail.com>
2023-12-04 16:19:10 +0100

    day 2

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,49 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+type Grab = { red?: number; green?: number; blue?: number };
+
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  const required = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
+  const matchingGameIds: number[] = [];
+
+  for (let i = 0; i < data.length; i++) {
+    if (!data[i]) {
+      continue;
+    }
+
+    const [game, grabsRaw] = data[i].split(':');
+    const grabs: Grab[] = grabsRaw.split(';').map((x) => {
+      const grab = {};
+      let result;
+      const regexp = /([0-9]+)\s(blue|red|green)/gi;
+      while ((result = regexp.exec(x)) !== null) {
+        grab[result[2] as 'red' | 'green' | 'blue'] = parseInt(result[1], 10);
+      }
+      return grab;
+    });
+
+    if (
+      grabs.every((grab) => {
+        const redOk = !grab.red || grab.red <= required.red;
+        const greenOk = !grab.green || grab.green <= required.green;
+        const blueOk = !grab.blue || grab.blue <= required.blue;
+
+        return redOk && blueOk && greenOk;
+      })
+    ) {
+      matchingGameIds.push(parseInt(game.split('Game ')[1], 10));
+    }
+  }
+
+  console.log(matchingGameIds);
+
+  return matchingGameIds.reduce((a, b) => a + b, 0);
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -1,9 +1,41 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+type Grab = { red?: number; green?: number; blue?: number };
+
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let total = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    if (!data[i]) {
+      continue;
+    }
+
+    const [game, grabsRaw] = data[i].split(':');
+    const grabs: Grab[] = grabsRaw.split(';').map((x) => {
+      const grab = {};
+      let result;
+      const regexp = /([0-9]+)\s(blue|red|green)/gi;
+      while ((result = regexp.exec(x)) !== null) {
+        grab[result[2] as 'red' | 'green' | 'blue'] = parseInt(result[1], 10);
+      }
+      return grab;
+    });
+
+    const minimum = {
+      red: grabs.reduce((v, g) => (g.red && g.red > v ? g.red : v), 0),
+      green: grabs.reduce((v, g) => (g.green && g.green > v ? g.green : v), 0),
+      blue: grabs.reduce((v, g) => (g.blue && g.blue > v ? g.blue : v), 0),
+    };
+    const power = minimum.red * minimum.green * minimum.blue;
+
+    console.log(minimum, power);
+
+    total += power;
+  }
+
+  return total;
 }
 
 const answer = await day2b();
