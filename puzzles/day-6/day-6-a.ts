2175068606a4480c0ee1f34f7c963c5394cdff17
Klaas Cuvelier <cuvelierklaas@gmail.com>
2023-12-06 09:22:32 +0100

    day 6

--- puzzles/day-6/day-6-a.ts
+++ puzzles/day-6/day-6-a.ts
@@ -3,7 +3,42 @@ import chalk from 'chalk';
 
 export async function day6a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const timeData = getLineValues(data[0]);
+  const distanceData = getLineValues(data[1]);
+  let answer = 0;
+
+  for (let index = 0; index < timeData.length; index++) {
+    const time = timeData[index];
+    const distance = distanceData[index];
+    let waysToWin = 0;
+
+    for (let chargeTime = 0; chargeTime < time; chargeTime++) {
+      // chargeTime is also speed
+      const speed = chargeTime;
+      const raceTime = time - chargeTime;
+      const distanceTraveled = raceTime * speed;
+
+      // win
+      if (distanceTraveled > distance) {
+        waysToWin++;
+      }
+    }
+
+    if (waysToWin > 0) {
+      answer = (answer === 0 ? 1 : answer) * waysToWin;
+    }
+  }
+
+  return answer;
+}
+
+function getLineValues(line: string): number[] {
+  return line
+    .split(':')[1]
+    .split(' ')
+    .filter((i) => i.length)
+    .map(Number);
 }
 
 const answer = await day6a();
--- puzzles/day-6/day-6-b.ts
+++ puzzles/day-6/day-6-b.ts
@@ -3,7 +3,29 @@ import chalk from 'chalk';
 
 export async function day6b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const time = getLineValue(data[0]);
+  const distance = getLineValue(data[1]);
+
+  let waysToWin = 0;
+
+  for (let chargeTime = 0; chargeTime < time; chargeTime++) {
+    // chargeTime is also speed
+    const speed = chargeTime;
+    const raceTime = time - chargeTime;
+    const distanceTraveled = raceTime * speed;
+
+    // win
+    if (distanceTraveled > distance) {
+      waysToWin++;
+    }
+  }
+
+  return waysToWin;
+}
+
+function getLineValue(line: string): number {
+  return Number(line.split(':')[1].replace(/\s/g, ''));
 }
 
 const answer = await day6b();
