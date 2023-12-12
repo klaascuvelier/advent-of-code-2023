29d97dbe7f968bd106b2ba59b635f94ba827ba69
Klaas Cuvelier <cuvelierklaas@gmail.com>
2023-12-12 19:44:07 +0100

    day 8 pt 1

--- puzzles/day-8/day-8-a.ts
+++ puzzles/day-8/day-8-a.ts
@@ -3,7 +3,30 @@ import chalk from 'chalk';
 
 export async function day8a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const instructions = data[0].split('');
+  const maps = new Map<string, { L: string; R: string }>();
+  const regexp = /([A-Z]+)\s=\s\(([A-Z]+),\s([A-Z]+)\)/i;
+
+  for (let i = 2; i < data.length; i++) {
+    if (data[i]) {
+      const [, identifier, left, right] = data[i].match(regexp);
+      maps.set(identifier, { L: left, R: right });
+    }
+  }
+
+  let pos = 'AAA';
+  let index = 0;
+  while (pos !== 'ZZZ') {
+    console.log(pos);
+    const instruction = instructions[index % instructions.length];
+    const { L, R } = maps.get(pos)!;
+    pos = instruction === 'L' ? L : R;
+    index++;
+  }
+  console.log(pos, index);
+
+  return index;
 }
 
 const answer = await day8a();
